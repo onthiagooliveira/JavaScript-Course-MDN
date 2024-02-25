bbf3a248e6fc0321de25f27ae360b6cbf3a212ae
onthiagooliveira <onthiagooliveira@outlook.com>
2024-02-25 20:06:01 -0300

    update folders

---
+++ 5-Metodos Uteis de Strings/metodosUteisString.js
@@ -0,0 +1,14 @@
+// Métodos úteis de String
+var browserType1 = "Mozilla";
+
+console.log(
+  browserType1.length   // retorna o comprimento da string
+); 
+
+// Acessano um caractere específico da string
+console.log(
+  browserType1[0]  // retorna a primeira letra
+);
+console.log(
+  browserType1[browserType1.length - 1]   // retorna a última letra
+);
