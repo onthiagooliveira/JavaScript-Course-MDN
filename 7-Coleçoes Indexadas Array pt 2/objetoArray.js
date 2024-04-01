8ef73fbc678562293b8f385dc1f6147ed225d47e
onthiagooliveira <onthiagooliveira@outlook.com>
2024-04-01 12:11:13 -0300

    update objetoArray studing

--- 7-Coleçoes Indexadas Array pt 2/objetoArray.js
+++ 7-Coleçoes Indexadas Array pt 2/objetoArray.js
@@ -1,27 +1,38 @@
 // As declarações à seguir criam Arrays equivalentes
-var bandas = new Array ('Lynyrd Skynyrd', 'Black Sabah');
-var games = Array ('Red Dead Redemption', 'The Last of Us');
-var hardware = ['Monitor', 'Teclado'];
-
+var bandas = new Array("Lynyrd Skynyrd", "Black Sabah");
+var games = Array("Red Dead Redemption", "The Last of Us");
+var hardware = ["Monitor", "Teclado"]; // Array Literals
 
 // Criando Array com tamanho diferente de zero mas sem nenhum item, qualquer esquema abaixo pode ser utilizado:
 var coffes = new Array(5);
-var  country = Array(193)
+var country = Array(193);
 
 // Estes produzem o mesmo efeito que:
 var books = [];
 books.length = 5;
 
-
 // Criando um Array como propriedade de um objeto
 var obj = {};
-obj.prop = ['Mouse', 'Fones'];
+obj.prop = ["Mouse", "Fones"];
 
 // O mesmo que
-var obj_2 = {prop: ['Coooler', 'NVME']};
-
+var obj_2 = { prop: ["Coooler", "NVME"] };
 
 /* Se desejarmos iniciar um Array com um único elemento, e este elemento é um número, deveremos utlizar a sintaxe dos colchetes */
 var listPecas = [35];
 
 // Povoando um array
+var employee = [];
+employee[0] = "Casey Jhones";
+employee[1] = "Phil Jhones";
+employee[2] = "August West";
+
+// Povoando um array logo quando o criamos
+var myArray = new Array("Hello", "myVar", 3.14);
+var fruits = ["Mongo", "Litter", "Lemon"];
+
+//Referenciando os elementos do Array com
+fruits[2];  // Return 'Lemon'
+
+// Referenciando os elementos do Array com o operador do array
+fruits['2'];  // Return 'Lemon'
